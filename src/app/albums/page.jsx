118d7afdb9fe34a6c7e5152e1e4f92fc677f9466
“willsmores” <scwillsmore@gmail.com>
2024-01-30 11:35:27 -0500

    Add some basic routing

---
+++ src/app/albums/page.jsx
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function AlbumsPage() {
+  return (
+    <div className={styles.main}>
+      <h1>This is the AlbumsPage</h1>
+    </div>
+  );
+}
---
+++ src/app/photos/page.jsx
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function PhotosPage() {
+  return (
+    <div className={styles.main}>
+      <h1>This is the PhotosPage</h1>
+    </div>
+  );
+}
