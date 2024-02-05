9bcd1bf14c281105044651ff5ec925710acc2d8d
“willsmores” <scwillsmore@gmail.com>
2024-02-05 18:12:23 -0500

    Add db schema for albums and photos

--- src/db/schema.mjs
+++ src/db/schema.mjs
@@ -9,12 +9,30 @@ const client = createClient({
 // Create users db schema
 const schema = `
   DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS albums;
 
   CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL
+  );
+
+  CREATE TABLE albums (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  );
+
+  CREATE TABLE photos (
+    id INTEGER PRIMARY KEY,
+    album_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
   );
 `;
 
